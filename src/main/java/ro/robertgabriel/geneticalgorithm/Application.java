19815b5b34e4e89ba53dc8c29ae8c9757359c317
Robert  Dinu <vjroby@gmail.com>
2018-05-05 12:12:27 +0700

    Changed package and added sonarqube.

---
+++ src/main/java/ro/robertgabriel/geneticalgorithm/Application.java
@@ -0,0 +1,11 @@
+package ro.robertgabriel.geneticalgorithm;
+
+public class Application {
+    public static void main(String[] args) {
+        String solution = "yruihfbjc4312rde";
+
+        String find = Generate.findSolution(10000, solution);
+        find = find == null ? "NONE" : find;
+        System.out.println("Find: " + find);
+    }
+}
